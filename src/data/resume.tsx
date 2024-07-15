8c03ed6fefa3cc36dde62ff21811fadf80186aa7
Ash0w0 <ashwinikkr007@gmail.com>
2024-07-15 12:43:26 +0530

    portfolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,325 @@
+import { Icons } from "@/components/icons";
+import { CodeIcon, HomeIcon, NotebookIcon, PencilLine } from "lucide-react";
+export const DATA = {
+  name: "Ashwini Kumar",
+  initials: "AK",
+  url: "https://ashwinikumar.tech/",
+  location: "Gurgaon , India",
+  locationLink: "https://www.google.com/maps/place/India",
+  description:
+    "Marketer turned Business development specialist with a knack of technical knowledge ",
+  summary:
+    "I'm Ashwini Kumar, a dedicated Business Development and Marketing Specialist with over three years of experience across various domains. My journey has taken me from a search engine marketing specialist in a news channel to a marketing role in a blockchain organization, transforming the way these organizations achieve their goals. Currently, I'm a Business Development Manager at HashTrust Technologies Pvt Ltd, where I've successfully conducted staff augmentation, built and managed cold email automated pipelines, and implemented LinkedIn marketing strategies. These efforts have significantly increased our website traffic and client acquisition rates. I pride myself on my negotiation skills and ability to close deals that drive revenue growth and market expansion. My communication and persuasion skills enable me to clearly articulate complex concepts and effectively influence decision-makers. Outside of my professional work, I volunteer with NGOs, helping students from underprivileged areas access education and working on cleanliness and beautification drives. I'm passionate about leveraging my skills to drive further growth and success in a forward-thinking organization. Let's connect and drive the future together!",
+  avatarUrl: "/me.png",
+  skills: [
+    "Lead Generation",
+    "Market Research and Analysis",
+    "Client Relationship Management",
+    "Strategic Planning",
+    "Sales Funnel Management",
+    "Networking",
+    "Proposal Writing",
+    "SEO",
+    "On-Page SEO",
+    "Off-Page SEO",
+    "Technical SEO",
+    "Keyword Research",
+    "Meta Tags Generation",
+    "Backlinks Building",
+    "Social Media Marketing",
+    "Social Media Optimization",
+    "Community Building",
+    "Online Traffic Analysis",
+    "Email Marketing",
+    "WordPress",
+    "LinkedIn Lead Generation",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Python",
+    "Java"
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    { href: "#", icon: CodeIcon, label: "Projects" },
+    { href: "#", icon: PencilLine, label: "Notes" },
+  ],
+  contact: {
+    email: "ashwinikkr007@gmail.com",
+    tel: "+918002822133",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/Ash0w0",
+        icon: Icons.github,
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/ashwini-kumarr/",
+        icon: Icons.linkedin,
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://www.linkedin.com/in/ashwini-kumarr/",
+        icon: Icons.x,
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://www.youtube.com/@adaptwithai",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#mailto:ashwinikkr007@gmail.com",
+        icon: Icons.email,
+        navbar: false,
+      },
+    },
+  },
+  work: [
+      {
+        company: "HashTrust Technologies Pvt Ltd",
+        href: "https://hashtrust.in",
+        badges: [],
+        location: "Gurgaon",
+        title: "Business Development Manager",
+        logoUrl: "/hashtrust.png",
+        start: "September 2022",
+        end: "Present",
+        description: `
+        Lead Generation and Marketing:<br>
+        - Successfully conducted staff augmentation for Hashtrust.<br>
+        - Built and managed cold email automated pipelines.<br>
+        - Implemented LinkedIn marketing strategies for business development.<br>
+        - Increased website traffic for hashtrust.in through effective SEO and marketing techniques.<br>
+        - Implemented SEO practices and ranked keywords on various search engines.<br><br>
+    
+        Data and Contact Management:<br>
+        - Conducted LinkedIn searches for company profiles and generated leads based on requirements.<br>
+        - Performed data analysis, company profiling, and industry profiling.<br>
+        - Searched for emails, contacts, direct numbers, and validated emails.<br>
+        - Maintained and updated lead status.<br><br>
+    
+        Staff Augmentation:<br>
+        - Used data gathered during application for staff augmentation to conduct A/B testing on different types of applications.<br>
+        - Increased the efficiency of the preexisting staff augmentation approach.<br>
+        - Increased lead conversion by understanding the exact requirements of clients and the offerings of our company and made personalization of our outreach strategy.<br>
+      `
+      },
+    {
+      company: "PrimaFelicitas",
+      href: "https://primafelicitas.com",
+      badges: [],
+      location: "Gurgaon",
+      title: "Digital Marketing Manager",
+      logoUrl: "/primafelicitas.png",
+      start: "May 2022",
+      end: "August 2022",
+      description:`    - Conducted LinkedIn searches for company profiles and generated leads based on requirements.<br>
+      - Performed data analysis, company profiling, and industry profiling.<br>
+      - Searched for emails, contacts, direct numbers, and validated emails.<br>
+      - Maintained and updated lead status.<br><br>
+  `
+    },
+    {
+      company: "ITV Network",
+      href: "https://itvnetwork.com",
+      badges: [],
+      location: "Gurgaon",
+      title: "Search Engine Optimization Executive",
+      logoUrl: "/itv.png",
+      start: "December 2021",
+      end: "April 2022",
+      description: `
+      - Conducted LinkedIn searches for company profiles and generated leads based on requirements.<br>
+      - Performed data analysis, company profiling, and industry profiling.<br>
+      - Searched for emails, contacts, direct numbers, and validated emails.<br>
+      - Maintained and updated lead status.<br>
+      - Collaborated with marketing and sales teams for lead generation tasks.
+    `
+      },
+    {
+      company: "Baazi Games",
+      href: "https://baazigames.com",
+      badges: [],
+      location: "Delhi",
+      title: "Search Engine Optimization Executive",
+      logoUrl: "/baazi.png",
+      start: "September 2021",
+      end: "December 2021",
+      description:
+      `
+      - Collaborated in a team with agile methodology for on-page and off-page optimization.<br>
+      - Conducted technical analysis, content planning, website audit, competitor analysis, and keyword research.<br>
+      - Worked on daily and weekly analysis of current ongoing SEO practices, maintained a checklist for all on-page, off-page, and technical SEO practices, and inferred potential recommendations.
+    `    }
+  ],
+  education: [
+    {
+      school: "Manipal Institute of Technology",
+      href: "https://manipal.edu/mit.html",
+      degree: "Bachelor of Technology - BTech, Civil Engineering",
+      logoUrl: "/mit_logo.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "SelaQui International School, Dehradun",
+      href: "https://selaqui.org",
+      degree: "12, Science",
+      logoUrl: "/selaqui_logo.png",
+      start: "2013",
+      end: "2015",
+    },
+    {
+      school: "St. Karen's High School",
+      href: "http://stkarenhighschool.com",
+      degree: "Schooling",
+      logoUrl: "/stkaren_logo.png",
+      start: "2002",
+      end: "2013",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Startup Mahakumbh",
+      dates: "March 18th - 20th, 2024",
+      location: "Bharat Manadapam, New Delhi",
+      description:
+        " Attended startup Mahakumbh event and talked with various startups and inquired about their business , Business models , marketing tactics and Business outlooks",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+  ],
+} as const;
